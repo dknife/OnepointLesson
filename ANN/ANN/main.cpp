1309a5589fad0c891f9da7a9b9feefe89b4e073a
dknife <ymkang@San302-YMKang.local>
2015-10-23 10:55:30 +0900

    ANN

---
+++ ANN/ANN/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ANN
+//
+//  Created by young-min kang on 10/15/15.
+//  Copyright © 2015 young-min kang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
